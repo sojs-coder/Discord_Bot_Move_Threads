c5b6a389efbec5bcd6c451b191b801ea56a2d7e7
SoJS coder <matfuller703@gmail.com>
2023-05-23 20:52:35 -0700

    Update discord_bot.py
    fixed a couple things with permisions

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,18 @@
 import discord
 from discord.ext import commands
+import os
 
-intents = discord.Intents.default()
+TOKEN = os.environ['TOKEN']
+
+
+intents = discord.Intents.all()
+print(intents)
 intents.message_content = True
-intents.message_history = True
+intents.guild_messages = True
+intents.messages = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -14,16 +21,19 @@ async def on_ready():
 @bot.command()
 async def move(ctx, num_messages: int, thread_name: str):
     # Get the last num_messages messages from the channel
-    messages = await ctx.channel.history(limit=num_messages + 1).flatten()
+    messages = []
+    async for message in ctx.channel.history(limit=num_messages + 1):
+        messages.append(message)
     
-    # Create a new thread
-    thread = await ctx.channel.create_thread(name=thread_name)
+    
     
     # Delete the last num_messages messages from the channel
-    await ctx.channel.purge(limit=num_messages + 1)
-    
+    await ctx.channel.delete_messages(messages);
+    # Create a new thread
+    thread = await ctx.channel.create_thread(name=thread_name)
     # Move the messages to the new thread
     for message in messages[:-1]:
-        await thread.send(content=message.content, username=message.author.name)
+        content = message.author.mention + " said: \n" + message.content
+        await thread.send(content=content)
 
-bot.run('YOUR_BOT_TOKEN')
+bot.run(TOKEN)
